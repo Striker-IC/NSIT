e31ba2cc450af70efb4971014030c66f7a96ae0e
Striker-IC <isseylg@outlook.fr>
2024-10-01 12:34:40 +0200

    Add files via upload

--- documents/space.py
+++ documents/space.py
@@ -1,22 +1,62 @@
 import pygame  # necessaire pour charger les images et les sons
+import random  # necessaire pour faire apparaitre les ennemis de manière aléatoire
 
-rapidité=1
 class Joueur() : # classe pour créer le vaisseau du joueur
     def __init__(self) :
-        self.sens="centre"
-        self.image="vaisseau.png"
-        self.position=368
+        self.position = 400
+        self.image = pygame.image.load("vaisseau.png")
+        self.sens = "O"
+        self.vitesse = 5
+        self.score = 0
+
+    def deplacer(self) :
+        if (self.sens == "droite") and (self.position < 740):
+            self.position = self.position + self.vitesse
+        elif (self.sens == "gauche") and (self.position > 0):
+           self.position = self.position - self.vitesse
+           
+    def tirer(self):
+        self.sens = "O"
+
+class Balle() :
+    def __init__(self, tireur):
+        self.tireur = tireur
+        self.depart = tireur.position + 16
+        self.hauteur = 492
+        self.image = pygame.image.load("balle.png")
+        self.etat = "chargee"
+        
+    def bouger(self):    
+        if self.etat == "chargee":
+                self.depart = self.tireur.position + 16
+                self.hauteur = 492
+        elif self.etat == "tiree" :
+                self.hauteur = self.hauteur - 5    
+        if self.hauteur < 0:
+            self.etat = "chargee"
+            
+class Ennemi(self):
+    NbEnnemis = 0
+    def __init__(self, player):
+        self.depart= random.randint(0, 800-64)
+        self.hauteur= 64
+        self.type= 0
+        self.image = pygame.image.load("invader1.png")
+        self.vitesse= 1
     
-    def deplacer(self):
-        if player.sens == "gauche":
-            self.position -= 0,5
-            if self.position < 0:
-                self.position = 0
+    def avancer(self):
+        self.hauteur = self.hauteur + 2
+        if self.hauteur > 600:
+            self.hauteur = 64
+            self.hauteur = self.hauteur + 2
+        
                 
-        if player.sens == "droite":
-            self.position += rapidité
-            if self.position > 772:
-                self.position = 772
+            
+        
+        
+        
+    
+    
                 
         
         
